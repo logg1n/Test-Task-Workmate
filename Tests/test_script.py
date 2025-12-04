18f346a14a13691c19e6d378e3c8e358f90cfb6e
logg1n <loggiin13@gmail.com>
2025-12-04 10:22:36 +0300

    create_csv_file переписана с макером @pytest.fixture

--- Tests/test_script.py
+++ Tests/test_script.py
@@ -4,20 +4,21 @@ import pytest
 
 from script import analysis_of_developer_performance
 from table import Table
+@pytest.fixture
+def create_csv_file():
+    def _create(headers, rows):
+        with tempfile.NamedTemporaryFile(
+            mode="w+", newline="", delete=False, encoding="utf-8"
+        ) as tmp:
+            writer = csv.writer(tmp)
+            writer.writerow(headers)
+            writer.writerows(rows)
+            tmp.flush()
+        return tmp.name
+    return _create
 
 
-def create_csv_file(headers, rows):
-    with tempfile.NamedTemporaryFile(
-        mode="w+", newline="", delete=False, encoding="utf-8"
-    ) as tmp:
-        writer = csv.writer(tmp)
-        writer.writerow(headers)
-        writer.writerows(rows)
-        tmp.flush()
-    return tmp.name
-
-
-def test_analysis_basic():
+def test_analysis_basic(create_csv_file):
     file = create_csv_file(
         ["position", "performance"],
         [["Backend Developer", "4.8"], ["QA Engineer", "4.5"]],
@@ -30,7 +31,7 @@ def test_analysis_basic():
     ]
 
 
-def test_analysis_no_keys():
+def test_analysis_no_keys(create_csv_file):
     file = create_csv_file(["position", "performance"], [["Backend Developer", "4.8"]])
     table = analysis_of_developer_performance([file], keys=["Salary"])
     # таблица создаётся с пустыми заголовками
@@ -38,7 +39,7 @@ def test_analysis_no_keys():
     assert table.get_rows()[0] == []
 
 
-def test_analysis_multiple_files():
+def test_analysis_multiple_files(create_csv_file):
     file1 = create_csv_file(["position", "performance"], [["Backend Developer", "4.8"]])
     file2 = create_csv_file(["position", "performance"], [["QA Engineer", "4.5"]])
     table = analysis_of_developer_performance(
@@ -58,7 +59,7 @@ def test_analysis_empty_files():
     assert table is None
 
 
-def test_analysis_with_numeric_and_text():
+def test_analysis_with_numeric_and_text(create_csv_file):
     file = create_csv_file(
         ["position", "performance", "Level"],
         [["Backend Developer", "4.8", "Senior"], ["QA Engineer", "4.5", "Junior"]],
@@ -73,7 +74,7 @@ def test_analysis_with_numeric_and_text():
     ]
 
 
-def test_analysis_without_keys():
+def test_analysis_without_keys(create_csv_file):
     file = create_csv_file(
         ["position", "performance"],
         [["Backend Developer", "4.8"]],
@@ -84,7 +85,7 @@ def test_analysis_without_keys():
         ["Backend Developer", 4.8],
     ]
 
-def test_analysis_mixed_values():
+def test_analysis_mixed_values(create_csv_file):
     file = create_csv_file(
         ["position", "performance"],
         [
@@ -94,8 +95,7 @@ def test_analysis_mixed_values():
         ],
     )
     table = analysis_of_developer_performance([file], keys=["position", "performance"])
-    print("\n",table)
-    # ожидаем, что значения сохранятся как есть (список)
+
     assert table.get_rows() == [
         ["position", "performance"],
         ["Backend Developer", 4.0],
